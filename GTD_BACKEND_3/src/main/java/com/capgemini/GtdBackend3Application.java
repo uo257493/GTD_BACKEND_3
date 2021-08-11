5d171a9624805a194f02f4e5838b4c62022b9b3f
jardurag <uo257493@uniovi.es>
2021-08-11 08:25:36 +0200

    Commit inicial

---
+++ GTD_BACKEND_3/src/main/java/com/capgemini/GtdBackend3Application.java
@@ -0,0 +1,13 @@
+package com.capgemini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GtdBackend3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GtdBackend3Application.class, args);
+	}
+
+}
---
+++ GTD_BACKEND_3/src/test/java/com/capgemini/GtdBackend3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capgemini;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GtdBackend3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
